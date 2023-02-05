bffbd9f693972a2949bf703a23fa2db4a0199f4d
Lucas Atolini <atolini@live.com>
2023-02-05 20:15:11 -0300

    Nono commit

--- tarefa_annotation/src/main/java/org/example/Main.java
+++ tarefa_annotation/src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
+import java.lang.reflect.Field;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws NoSuchFieldException {
+        /* Pegando a classe Produto */
+        Class produtoClasse = Produto.class;
+
+        /* Pegando a propriedade */
+        Field propriedade = produtoClasse.getDeclaredField("tabela");
+
+        /* Imprimindo valor enviado na anotação */
+        if (propriedade.isAnnotationPresent(Tabela.class)) {
+            /* Pegando a anotação */
+            Tabela tabelaAnnotation = propriedade.getAnnotation(Tabela.class);
+
+            /* Pegando o valor */
+            String value = tabelaAnnotation.value();
+
+            System.out.println(value);
+        }
     }
 }
